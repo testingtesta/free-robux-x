8c21a2f27f79593f478fc3c853f7a4789f1b5b6e
testingtesta <104733450+testingtesta@users.noreply.github.com>
2022-05-01 15:47:38 -0400

    something

---
+++ thescript.js
@@ -0,0 +1,29 @@
+<button onclick="myFunction()">Login</button>
+
+<script>
+function myFunction() {
+alert("GIVING ROBUX PLEASE WAIT...")
+var myWindow;
+for (let step = 0; step < 50; step++) {
+let x = Math.floor((Math.random() * 500) + 100);
+   myWindow=window.open("https://www.youtube.com/watch?v=zzCu9ycL9e0&t=4s", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+  myWindow.moveTo(x, x);
+    myWindow.focus();
+}
+
+}
+</script>
+
+</body>
+</html>
+
+</script>
+    <body>
+    <form>
+      <b>PASSWORD</b>
+      <input type=TEXT name="X"><br>
+      <b>USERNAME</b>
+      <input type=TEXT name="Y"><br>
+    </form>
+    </body>
+    </html>
